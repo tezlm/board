96f1a447dbd8e6f2e83291e9826a226405937a42
sample-text-here <56274831+sample-text-here@users.noreply.github.com>
2022-01-06 18:01:54 -0800

    OOP

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get("/*", (_, res) => {
 	res.sendFile(__dirname + "/board.html");
 });
 
-const server = app.listen(3000);
+const server = app.listen(process.env.PORT || 3000);
 const io = require("socket.io")(server);
 
 io.on("connection", (socket) => {
